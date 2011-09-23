67066b4942c8db18d88f262656c1aad6f4dafd9a
Giovanni Bassi <giggio@giggio.net>
2011-09-23 20:04:01 -0300

    deixando tudo sincrono

--- ClientApp/ClientApp.vb
+++ ClientApp/ClientApp.vb
@@ -1,17 +1,15 @@
-﻿Imports System.Threading.Tasks
-Imports ClientApp.WCFService
+﻿Imports ClientApp.WCFService
 
 Module ClientApp
 
     Sub Main()
-        Console.WriteLine("Wait for the stock prices to be printed out or hit ENTER to exit...")
+        Console.WriteLine("Wait for the stock prices to be printed out...")
 
         Dim tickers As String() = {"MSFT", "C", "YHOO", "GOOG", "GE", "FOO"}
         While True
             Dim watch = New Stopwatch
             watch.Start()
-            Dim task = ComputeStockPricesAsync(tickers)
-            task.Wait()
+            ComputeStockPrices(tickers)
             watch.Stop()
             Console.WriteLine("All the stock prices have been obtained, press enter to obtain again, any other key to exit.")
             Console.WriteLine("Total time: " & Convert.ToDouble(watch.ElapsedMilliseconds) / 1000)
@@ -21,14 +19,14 @@ Module ClientApp
         End While
     End Sub
 
-    Async Function ComputeStockPricesAsync(ByVal tickers As String()) As Task
-        Dim quotes = Await New QuoteCalculatorServiceClient().GetQuotesAsync(tickers)
+    Sub ComputeStockPrices(ByVal tickers As String())
+        Dim quotes = New QuoteCalculatorServiceClient().GetQuotes(tickers)
         For Each quote In quotes
             Console.WriteLine("Ticker: " + quote.Ticker)
             Console.WriteLine(vbTab & "Price: " + If(Not quote.Price.Equals(0.0), quote.Price.ToString(), "Unknown"))
             Console.WriteLine(vbTab & "Change of the day: " + If(Not quote.Change.Equals(0.0), quote.Change.ToString(), "Unknown"))
             Console.WriteLine()
         Next
-    End Function
+    End Sub
 
 End Module
--- WCFQuoteCalculatorService/QuoteCalculatorService.svc.vb
+++ WCFQuoteCalculatorService/QuoteCalculatorService.svc.vb
@@ -1,26 +1,24 @@
 ﻿Imports System.Net
-Imports System.Threading.Tasks
 Imports System.Collections.ObjectModel
 
 Public Class QuoteCalculatorService
     Implements IQuoteCalculatorService
 
-    Public Async Function GetQuoteAsync(ByVal ticker As String) As Task(Of Quote) Implements IQuoteCalculatorService.GetQuoteAsync
+    Public Function GetQuote(ByVal ticker As String) As Quote Implements IQuoteCalculatorService.GetQuote
         Dim price As Double
         Dim change As Double
 
-        Dim quoteValues As String() = (Await New WebClient().DownloadStringTaskAsync("http://download.finance.yahoo.com/d/quotes.csv?s=" & ticker & "&f=l1c1n")).Split(",")
+        Dim quoteValues As String() = New WebClient().DownloadString("http://download.finance.yahoo.com/d/quotes.csv?s=" & ticker & "&f=l1c1n").Split(",")
         Double.TryParse(quoteValues(0), price)
         Double.TryParse(quoteValues(1), change)
 
-        Await Task.Delay(2000)
+        System.Threading.Thread.Sleep(2000)
 
         Return New Quote() With {.Ticker = ticker, .Price = price, .Change = change}
     End Function
 
-    Public Async Function GetQuotesAsync(ByVal tickers As String()) As Task(Of ReadOnlyCollection(Of Quote)) Implements IQuoteCalculatorService.GetQuotesAsync
-        Return New ReadOnlyCollection(Of Quote)(
-            Await Task.WhenAll(From ticker In tickers Select GetQuoteAsync(ticker)))
+    Public Function GetQuotesAsync(ByVal tickers As String()) As ReadOnlyCollection(Of Quote) Implements IQuoteCalculatorService.GetQuotes
+        Return New ReadOnlyCollection(Of Quote)((From ticker In tickers Select GetQuote(ticker)).ToList())
     End Function
 
 End Class
